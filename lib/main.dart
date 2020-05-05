69c308a98d61560ddc08e035a327361194a90a70
Roberto Ribeiro <robertov82008@gmail.com>
2020-05-05 16:52:21 -0300

    Step 7: Navigate to a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-          appBar: AppBar(title: Text('Starup Name Generator')),
-          body: Center(
-            child: RandomWords(),
-          )),
+      home: RandomWords(),
     );
   }
 }
@@ -26,6 +22,22 @@ class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
   final Set<WordPair> _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(builder: (BuildContext context) {
+        final Iterable<ListTile> tiles = _saved.map((WordPair pair) =>
+            ListTile(title: Text(pair.asPascalCase, style: _biggerFont)));
+
+        final List<Widget> divided =
+            ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+        return Scaffold(
+            appBar: AppBar(title: Text('Saved Suggestions')),
+            body: ListView(children: divided));
+      }),
+    );
+  }
 
   Widget _buildSuggestions() {
     return ListView.builder(
@@ -48,25 +60,36 @@ class RandomWordsState extends State<RandomWords> {
     final bool alreadySaved = _saved.contains(pair);
 
     return ListTile(
-      title: Text(pair.asPascalCase, style: _biggerFont),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
+        title: Text(pair.asPascalCase, style: _biggerFont),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
         });
-      }
-    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return _buildSuggestions();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Starup Name Generator'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.list),
+              onPressed: _pushSaved,
+            )
+          ],
+        ),
+        body: Center(
+          child: _buildSuggestions(),
+        ));
   }
 }
