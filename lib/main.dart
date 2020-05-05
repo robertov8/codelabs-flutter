73517368bc9e2f5107e0affe44cf8f0c03683fbf
Roberto Ribeiro <robertov82008@gmail.com>
2020-05-05 16:02:16 -0300

    Step 6: Add interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -45,14 +45,23 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildWord(WordPair pair) {
-    final bool already = _saved.contains(pair);
+    final bool alreadySaved = _saved.contains(pair);
 
     return ListTile(
       title: Text(pair.asPascalCase, style: _biggerFont),
       trailing: Icon(
-        already ? Icons.favorite : Icons.favorite_border,
-        color: already ? Colors.red : null,
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      }
     );
   }
 
